3e22d088d6f857cd63eddf8e8fc3ffdf6be0e155
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-20 12:34:48 +0300

    create login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:social_meda/core/services/get_it_service.dart';
+import 'package:social_meda/features/auth/presentation/views/login_page.dart';
 import 'package:social_meda/firebase_options.dart';
 
 void main() async {
@@ -14,8 +15,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const Scaffold(body: Center(child: Text('Hello World'))),
-    );
+    return MaterialApp(home: LoginPage());
   }
 }
