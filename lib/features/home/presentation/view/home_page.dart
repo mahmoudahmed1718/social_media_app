f4076b2212c6c29cc8101f9cc8ff7e93123e3279
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-20 19:32:37 +0300

    create home page body and add appbar and my drawer to home page

--- lib/features/home/presentation/view/home_page.dart
+++ lib/features/home/presentation/view/home_page.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:social_meda/features/home/presentation/view/widgets/home_page_body.dart';
+import 'package:social_meda/features/home/presentation/view/widgets/my_drawer.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(centerTitle: true, title: const Text("Social Media")),
+      body: HomePageBody(),
+      drawer: MyDrawer(),
+    );
   }
 }
---
+++ lib/features/home/presentation/view/widgets/home_page_body.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class HomePageBody extends StatelessWidget {
+  const HomePageBody({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
