c2e247e15f57ce7073b2b3a00abedc03cf7b7c3e
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-20 19:30:10 +0300

    add my drawer widget

---
+++ lib/features/home/presentation/view/widgets/my_drawer.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social_meda/features/auth/presentation/manger/cubit/auth_cubit.dart';
+import 'package:social_meda/features/home/presentation/view/widgets/my_drawer_tile.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            children: [
+              // logo
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 50),
+                child: Icon(
+                  Icons.person,
+                  size: 80,
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+              Divider(color: Theme.of(context).colorScheme.secondary),
+              // home tile
+              MyDrawerTile(
+                title: 'H O M E',
+                icon: Icons.home,
+                onTap: () => Navigator.pop(context),
+              ),
+              // profile tile
+              MyDrawerTile(
+                title: 'P R O F I L E',
+                icon: Icons.person,
+                onTap: () {
+                  Navigator.pop(context);
+                  // navigate to profile page
+                  final user = context.read<AuthCubit>().currentUser;
+                  String? uid = user!.uId;
+                  //! should Navigate to profile page
+                },
+              ),
+              // search tile
+              MyDrawerTile(
+                title: 'S E A R C H',
+                icon: Icons.search,
+                onTap: () {},
+              ),
+              //setting tile
+              MyDrawerTile(
+                title: 'S E T T I N G',
+                icon: Icons.settings,
+                onTap: () {},
+              ),
+              const Spacer(),
+              // logout tile
+              MyDrawerTile(
+                title: 'L O G O U T',
+                icon: Icons.login,
+                onTap: () {
+                  context.read<AuthCubit>().logout();
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
