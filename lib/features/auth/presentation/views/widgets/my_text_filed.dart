0dd6eef81b49116373a5b39ce8a7b712604f00aa
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-20 14:22:55 +0300

    create my text field and finsh this widget

---
+++ lib/features/auth/presentation/views/widgets/my_text_filed.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget {
+  const MyTextField({
+    super.key,
+    required this.textEditingController,
+    required this.hintText,
+    this.iconButton,
+    this.obscureText = false,
+  });
+  final TextEditingController textEditingController;
+  final String hintText;
+  final bool obscureText;
+  final IconButton? iconButton;
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: textEditingController,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        suffixIcon: iconButton,
+        hintStyle: TextStyle(color: Theme.of(context).colorScheme.primary),
+        fillColor: Theme.of(context).colorScheme.secondary,
+        filled: true,
+        hintText: hintText,
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Theme.of(context).colorScheme.tertiary),
+          borderRadius: BorderRadius.circular(12),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Theme.of(context).colorScheme.primary),
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+    );
+  }
+}
