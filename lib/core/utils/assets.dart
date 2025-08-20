3dd4d9e0a8cf76f3976c17e4997577c401c9b6c7
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-20 11:50:21 +0300

    create asset file for call image easy and modify it

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,9 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for assetsImagesAppLogo
+  /// assets/images/app_logo.jpg
+  static const String assetsImagesAppLogo = "assets/images/app_logo.jpg";
+}
+
