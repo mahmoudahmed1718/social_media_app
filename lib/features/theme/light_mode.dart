7fdedb0022b193f9735c03e62878cefc50ea8140
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-20 14:11:46 +0300

    create theme feature and add light mode file for colors of apps

---
+++ lib/features/theme/light_mode.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    surface: Colors.grey.shade300,
+    primary: Colors.grey.shade500,
+    secondary: Colors.grey.shade200,
+    tertiary: Colors.grey.shade100,
+    inversePrimary: Colors.grey.shade900,
+  ),
+  scaffoldBackgroundColor: Colors.grey.shade300,
+);
+
+// ThemeData darkMode = ThemeData(
+//   colorScheme: ColorScheme.dark(
+//     surface: Colors.grey.shade900,
+//     primary: Colors.grey.shade500,
+//     secondary: Colors.grey.shade200,
+//     tertiary: Colors.grey.shade100,
+//     inversePrimary: Colors.grey.shade900,
+//   ),
+//   scaffoldBackgroundColor: Colors.grey.shade900,
+// );
