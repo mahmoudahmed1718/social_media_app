a1b4621e13ae747665aefc481ce54e4a3d70977d
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-20 15:50:56 +0300

    add bloc consumer to resgster page body and creat file with it for refactor

--- lib/features/auth/presentation/views/register_page.dart
+++ lib/features/auth/presentation/views/register_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:social_meda/core/services/get_it_service.dart';
 import 'package:social_meda/features/auth/domain/repos/auth_repo.dart';
 import 'package:social_meda/features/auth/presentation/manger/cubit/auth_cubit.dart';
-import 'package:social_meda/features/auth/presentation/views/widgets/regster_page_body.dart';
+import 'package:social_meda/features/auth/presentation/views/widgets/regster_page_body_bloc_consumer.dart';
 
 class RegisterPage extends StatelessWidget {
   const RegisterPage({super.key});
@@ -12,7 +12,7 @@ class RegisterPage extends StatelessWidget {
     return Scaffold(
       body: BlocProvider(
         create: (context) => AuthCubit(authRepo: getIt.get<AuthRepo>()),
-        child: RegsterPageBody(),
+        child: RegsterPageBodyBlocConsumer(),
       ),
     );
   }
---
+++ lib/features/auth/presentation/views/widgets/regster_page_body_bloc_consumer.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social_meda/core/helper/snak_bar_method.dart';
+import 'package:social_meda/features/auth/presentation/manger/cubit/auth_cubit.dart';
+import 'package:social_meda/features/auth/presentation/views/widgets/regster_page_body.dart';
+
+class RegsterPageBodyBlocConsumer extends StatelessWidget {
+  const RegsterPageBodyBlocConsumer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<AuthCubit, AuthState>(
+      listener: (context, state) {
+        if (state is AuthError) {
+          snakBarMethod(context: context, message: state.message);
+        } else if (state is AuthSuccess) {
+          snakBarMethod(
+            context: context,
+            message: 'account created successfully',
+          );
+        }
+      },
+      builder: (context, state) {
+        return RegsterPageBody();
+      },
+    );
+  }
+}
