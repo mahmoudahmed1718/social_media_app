670f7ad1fe6cc3c4dd0101e6f863e58f271e04fa
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-20 14:07:51 +0300

    create login page body file

--- lib/features/auth/presentation/views/login_page.dart
+++ lib/features/auth/presentation/views/login_page.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:social_meda/features/auth/presentation/views/widgets/login_page_body.dart';
 
 class LoginPage extends StatelessWidget {
   static const routeName = 'login_page';
   const LoginPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(body: LoginPageBody());
   }
 }
---
+++ lib/features/auth/presentation/views/widgets/login_page_body.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class LoginPageBody extends StatelessWidget {
+  const LoginPageBody({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
