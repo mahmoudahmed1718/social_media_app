b482c2d45480c266a4beb3c8ed3b4bed63343ced
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-21 11:05:40 +0300

    refactor: integrate DatabaseService into AuthRepoImpl and update GetIt setup

--- lib/core/services/get_it_service.dart
+++ lib/core/services/get_it_service.dart
@@ -1,5 +1,7 @@
 import 'package:get_it/get_it.dart';
+import 'package:social_meda/core/services/database_service.dart';
 import 'package:social_meda/core/services/firebase_auth_service.dart';
+import 'package:social_meda/core/services/firestore_service.dart';
 import 'package:social_meda/features/auth/data/repos/auth_repo_impl.dart';
 import 'package:social_meda/features/auth/domain/repos/auth_repo.dart';
 
@@ -7,7 +9,11 @@ GetIt getIt = GetIt.instance;
 
 void setupGetIt() {
   getIt.registerSingleton<FirebaseAuthServices>(FirebaseAuthServices());
+  getIt.registerSingleton<DatabaseService>(FirestoreService());
   getIt.registerSingleton<AuthRepo>(
-    AuthRepoImpl(firebaseAuthServices: getIt.get<FirebaseAuthServices>()),
+    AuthRepoImpl(
+      firebaseAuthServices: getIt.get<FirebaseAuthServices>(),
+      databaseService: getIt.get<DatabaseService>(),
+    ),
   );
 }
--- lib/features/auth/data/repos/auth_repo_impl.dart
+++ lib/features/auth/data/repos/auth_repo_impl.dart
@@ -1,6 +1,8 @@
 import 'package:dartz/dartz.dart';
+
 import 'package:social_meda/core/errors/custom_excption.dart';
 import 'package:social_meda/core/errors/server_faileur.dart';
+import 'package:social_meda/core/services/database_service.dart';
 import 'package:social_meda/core/services/firebase_auth_service.dart';
 import 'package:social_meda/features/auth/data/model/app_user_model.dart';
 import 'package:social_meda/features/auth/domain/entites/app_user_entity.dart';
@@ -8,8 +10,12 @@ import 'package:social_meda/features/auth/domain/repos/auth_repo.dart';
 
 class AuthRepoImpl implements AuthRepo {
   final FirebaseAuthServices firebaseAuthServices;
+  final DatabaseService databaseService;
 
-  AuthRepoImpl({required this.firebaseAuthServices});
+  AuthRepoImpl({
+    required this.firebaseAuthServices,
+    required this.databaseService,
+  });
 
   @override
   Future<Either<ServerFaileur, AppUserEntity>> signInWithEmailAndPassword({
