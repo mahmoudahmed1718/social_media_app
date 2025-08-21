5f9a7ae2faa2ad4a51edb3528bd55cdb72fcf40b
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-21 11:00:22 +0300

    add usercollenction to backendpoint and make it abstract to not make any one create object from it

--- lib/core/services/backend_endpoint.dart
+++ lib/core/services/backend_endpoint.dart
@@ -1,5 +1,6 @@
-class BackendEndpoint {
+abstract class BackendEndpoint {
   static const String userUid = 'uId';
   static const String userName = 'name';
   static const String userEmail = 'email';
+  static const String usersCollection = 'users';
 }
