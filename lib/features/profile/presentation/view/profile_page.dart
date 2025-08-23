f3d5fa40752ad73c59b5474ca85845b5fecfeef9
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-23 11:23:34 +0300

    update: modify app bar title to display user's email in ProfilePage

--- lib/features/profile/presentation/view/profile_page.dart
+++ lib/features/profile/presentation/view/profile_page.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:social_meda/core/helper/get_user_data.dart';
 import 'package:social_meda/features/profile/presentation/view/widgets/profile_page_body.dart';
 
 class ProfilePage extends StatelessWidget {
   static const String routeName = '/profile';
   const ProfilePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Profile')),
+      appBar: AppBar(centerTitle: true, title: Text(getUserData().email)),
       body: const ProfilePageBody(),
     );
   }
