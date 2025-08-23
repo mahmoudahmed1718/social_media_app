5aab4059e44261313c72c5179e453c7ff50d5ba7
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-23 11:17:31 +0300

    create profile page body file

--- lib/features/profile/presentation/view/profile_page.dart
+++ lib/features/profile/presentation/view/profile_page.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:social_meda/features/profile/presentation/view/widgets/profile_page_body.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: const Text('Profile')),
+      body: const ProfilePageBody(),
+    );
   }
 }
---
+++ lib/features/profile/presentation/view/widgets/profile_page_body.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ProfilePageBody extends StatelessWidget {
+  const ProfilePageBody({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
