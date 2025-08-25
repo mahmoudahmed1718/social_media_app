ebad62b56f087a5a15bd0adf22d119930b9e0797
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-08-25 11:34:12 +0300

    feat: implement BlocBuilder in ProfilePageBodyBlocBuilder for improved state management

--- lib/features/profile/presentation/view/widgets/profile_page_body_bloc_builder.dart
+++ lib/features/profile/presentation/view/widgets/profile_page_body_bloc_builder.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:social_meda/features/profile/presentation/manger/cubit/profile_cubit.dart';
 import 'package:social_meda/features/profile/presentation/view/widgets/profile_page_body.dart';
 
 class ProfilePageBodyBlocBuilder extends StatelessWidget {
@@ -6,6 +8,18 @@ class ProfilePageBodyBlocBuilder extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ProfilePageBody();
+    return BlocBuilder<ProfileCubit, ProfileState>(
+      builder: (context, state) {
+        if (state is ProfileLoading) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (state is ProfileLoaded) {
+          return ProfilePageBody();
+        } else if (state is ProfileError) {
+          return Center(child: Text(state.message));
+        } else {
+          return const Center(child: Text('Please wait...'));
+        }
+      },
+    );
   }
 }
