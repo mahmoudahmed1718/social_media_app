dadba5ef792eb0502062d7a05233809da1209b0f
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-09-02 09:30:53 +0300

    feat: refactor keys management and initialize Supabase in main

--- lib/core/utils/keys.dart
+++ lib/core/utils/keys.dart
@@ -1,8 +1,7 @@
-// lib/core/utils/keys.dart
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class Keys {
-  static final String googleApiKey = dotenv.env['google_api_key']!;
-  // static final String supabaseUrl = dotenv.env['URL_SUPABASE']!;
-  // static final String supabaseApiKey = dotenv.env['API_KEY_SUPABASE']!;
+abstract class Keys {
+  static String get googleApiKey => dotenv.env['google_api_key']!;
+  static String get supabaseUrl => dotenv.env['supabase_url']!;
+  static String get supabaseAnonKey => dotenv.env['supabase_anon_key']!;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,25 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:social_meda/core/services/get_it_service.dart';
+import 'package:social_meda/core/utils/keys.dart';
 import 'package:social_meda/core/utils/on_route_generate.dart';
 
 import 'package:social_meda/features/splash/presentation/view/splash_view.dart';
 import 'package:social_meda/features/theme/light_mode.dart';
 import 'package:social_meda/firebase_options.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'core/services/shared_prefence_singleton.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Supabase.initialize(
+    url: Keys.supabaseUrl,
+    anonKey: Keys.supabaseAnonKey,
+  );
   setupGetIt();
   await SharedPreferenceSingleton.init();
   runApp(const MyApp());
