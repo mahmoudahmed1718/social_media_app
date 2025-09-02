4e6120def8bcb80c43a2e52132e67b3346654a58
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-09-02 09:40:56 +0300

    implemnt storage service and supabase storge in get_it_service to make calling it so easy

--- lib/core/services/get_it_service.dart
+++ lib/core/services/get_it_service.dart
@@ -2,6 +2,8 @@ import 'package:get_it/get_it.dart';
 import 'package:social_meda/core/services/database_service.dart';
 import 'package:social_meda/core/services/firebase_auth_service.dart';
 import 'package:social_meda/core/services/firestore_service.dart';
+import 'package:social_meda/core/services/storage_service.dart';
+import 'package:social_meda/core/services/supabase_storage_service.dart';
 import 'package:social_meda/features/auth/data/repos/auth_repo_impl.dart';
 import 'package:social_meda/features/auth/domain/repos/auth_repo.dart';
 import 'package:social_meda/features/profile/data/repo/profile_user_repo_impl.dart';
@@ -12,6 +14,7 @@ GetIt getIt = GetIt.instance;
 void setupGetIt() {
   getIt.registerSingleton<FirebaseAuthServices>(FirebaseAuthServices());
   getIt.registerSingleton<DatabaseService>(FirestoreService());
+  getIt.registerSingleton<StorageService>(SupabaseStorageService());
   getIt.registerSingleton<AuthRepo>(
     AuthRepoImpl(
       firebaseAuthServices: getIt.get<FirebaseAuthServices>(),
